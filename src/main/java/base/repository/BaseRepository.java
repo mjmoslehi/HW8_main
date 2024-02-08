58c03a627ad35a4709fbdf9bc2205b87464297f7
mamad <you@example.com>
2024-02-08 21:11:11 +0330

    create BaseRepository interface

---
+++ src/main/java/base/repository/BaseRepository.java
@@ -0,0 +1,11 @@
+package base.repository;
+
+import base.model.BaseEntity;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+
+public interface BaseRepository <ID extends Serializable , TYPE extends BaseEntity<ID>>{
+
+    void save(TYPE entity)throws SQLException;
+}
