f229da3789cd7a3ac42580559b0a43f3924b12f7
mamad <you@example.com>
2024-02-08 21:06:48 +0330

    add lombok to BaseEntity class

--- src/main/java/base/model/BaseEntity.java
+++ src/main/java/base/model/BaseEntity.java
@@ -1,6 +1,16 @@
 package base.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 
 public class BaseEntity <ID extends Serializable>{
 }
