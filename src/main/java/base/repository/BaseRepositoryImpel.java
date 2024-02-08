fee8e2b3dfccdc442ca16e3bc710302a5aa9e13f
mamad <you@example.com>
2024-02-08 21:16:08 +0330

    add generic to BaseRepositoryImpel

--- src/main/java/base/repository/BaseRepositoryImpel.java
+++ src/main/java/base/repository/BaseRepositoryImpel.java
@@ -1,4 +1,10 @@
 package base.repository;
 
-public class BaseRepositoryImpel {
+import base.model.BaseEntity;
+
+import java.io.Serializable;
+
+public abstract class  BaseRepositoryImpel <ID extends Serializable , TYPE extends BaseEntity<ID>>
+        implements BaseRepository<ID , TYPE>{
+
 }
