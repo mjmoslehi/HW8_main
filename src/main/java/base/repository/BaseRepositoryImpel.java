6ba4e17faf3e516009b89b2430010a35a7e83e9d
mamad <you@example.com>
2024-02-08 21:16:47 +0330

    add connection field to BaseRepositoryImpel

--- src/main/java/base/repository/BaseRepositoryImpel.java
+++ src/main/java/base/repository/BaseRepositoryImpel.java
@@ -3,8 +3,13 @@ package base.repository;
 import base.model.BaseEntity;
 
 import java.io.Serializable;
+import java.sql.Connection;
 
 public abstract class  BaseRepositoryImpel <ID extends Serializable , TYPE extends BaseEntity<ID>>
         implements BaseRepository<ID , TYPE>{
+
+    protected final Connection connection;
+
+
 
 }
