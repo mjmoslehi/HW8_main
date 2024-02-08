72581f1b0bd441e7926736e1e9b4c1dc0ba3eada
mamad <you@example.com>
2024-02-08 21:35:20 +0330

    add lombok to Category class

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -1,6 +1,14 @@
 package model;
 
 import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 
 public class Category extends BaseEntity<Integer> {
 }
