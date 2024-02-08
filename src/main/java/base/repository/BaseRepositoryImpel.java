6815d59f286afa10b5c8a487cdcf504e83adc1ff
mamad <you@example.com>
2024-02-08 21:18:18 +0330

    add override method to BaseRepositoryImpel

--- src/main/java/base/repository/BaseRepositoryImpel.java
+++ src/main/java/base/repository/BaseRepositoryImpel.java
@@ -4,6 +4,7 @@ import base.model.BaseEntity;
 
 import java.io.Serializable;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public abstract class  BaseRepositoryImpel <ID extends Serializable , TYPE extends BaseEntity<ID>>
         implements BaseRepository<ID , TYPE>{
@@ -13,4 +14,26 @@ public abstract class  BaseRepositoryImpel <ID extends Serializable , TYPE exten
     public BaseRepositoryImpel(Connection connection) {
         this.connection = connection;
     }
+
+    @Override
+    public void save(TYPE entity) throws SQLException {
+
+    }
+
+    @Override
+    public TYPE findById(ID id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(TYPE entity) throws SQLException {
+
+    }
+
+    @Override
+    public void delete(ID id) throws SQLException {
+
+    }
+
+
 }
