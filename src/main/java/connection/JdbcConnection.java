2ef6b282aec88191147c8429c9b610faebe8307a
mamad <you@example.com>
2024-02-08 21:29:53 +0330

    add getConnection method to JdbcConnection class

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,6 +1,8 @@
 package connection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class JdbcConnection {
 
@@ -8,4 +10,16 @@ public class JdbcConnection {
 
     private JdbcConnection(){}
 
+    public static Connection getConnection() {
+        if(connection == null){
+            try {
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                        "postgres",
+                        "mjm6yhn7ujm");
+            } catch (SQLException e) {
+                System.out.println("not found url");
+            }
+        }
+        return connection;
+    }
 }
