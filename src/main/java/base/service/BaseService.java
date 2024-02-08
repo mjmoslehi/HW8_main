ad9695abf82b5581b21ee1b61d7d9535ef4c87a9
mamad <you@example.com>
2024-02-08 21:22:37 +0330

    add save method to BaseService interface

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -3,6 +3,9 @@ package base.service;
 import base.model.BaseEntity;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 
 public interface BaseService <ID extends Serializable , TYPE extends BaseEntity<ID>> {
+
+    void save(TYPE entity) throws SQLException;
 }
