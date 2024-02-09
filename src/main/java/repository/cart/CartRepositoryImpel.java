d50a66254ebc63c7fac758208577808261876cb4
mamad <you@example.com>
2024-02-09 07:49:25 +0330

    add constructor to CartRepositoryImpel class

--- src/main/java/repository/cart/CartRepositoryImpel.java
+++ src/main/java/repository/cart/CartRepositoryImpel.java
@@ -3,5 +3,12 @@ package repository.cart;
 import base.repository.BaseRepositoryImpel;
 import model.Cart;
 
+import java.sql.Connection;
+
 public class CartRepositoryImpel extends BaseRepositoryImpel<Integer, Cart> implements CartRepository{
+
+    public CartRepositoryImpel(Connection connection) {
+        super(connection);
+    }
+
 }
