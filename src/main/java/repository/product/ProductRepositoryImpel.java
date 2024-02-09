283645a63642697e13bc2edd764ad3a6d7cbca08
mamad <you@example.com>
2024-02-09 08:01:32 +0330

    add loadAll method ProductRepositoryImpel class

--- src/main/java/repository/product/ProductRepositoryImpel.java
+++ src/main/java/repository/product/ProductRepositoryImpel.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ProductRepositoryImpel  extends BaseRepositoryImpel<Integer , Product> implements ProductRepository {
+public class ProductRepositoryImpel extends BaseRepositoryImpel<Integer, Product> implements ProductRepository {
 
     public ProductRepositoryImpel(Connection connection) {
         super(connection);
@@ -31,10 +31,10 @@ public class ProductRepositoryImpel  extends BaseRepositoryImpel<Integer , Produ
 
     @Override
     public void fillParamForStatement(PreparedStatement preparedStatement, Product entity, boolean isCountOnly) throws SQLException {
-        preparedStatement.setString(1,entity.getName());
-        preparedStatement.setInt(2,entity.getCost());
-        preparedStatement.setInt(3,entity.getNumber());
-        preparedStatement.setInt(4,entity.getCategoryId());
+        preparedStatement.setString(1, entity.getName());
+        preparedStatement.setInt(2, entity.getCost());
+        preparedStatement.setInt(3, entity.getNumber());
+        preparedStatement.setInt(4, entity.getCategoryId());
     }
 
     @Override
@@ -52,6 +52,42 @@ public class ProductRepositoryImpel  extends BaseRepositoryImpel<Integer , Produ
     @Override
     public String getUpdateQueryParams() {
         return "name =? , cost = ? , number = ? , category_id = ?";
+    }
+
+    @Override
+    public Product[] loadAll() throws SQLException {
+
+        String loadAll = "SELECT * FROM " + getTableName();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(loadAll,
+                ResultSet.TYPE_SCROLL_SENSITIVE,
+                ResultSet.CONCUR_UPDATABLE)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            int num = 0;
+
+            while (resultSet.next()) {
+                num++;
+            }
+
+            Product[] products = new Product[num];
+
+            resultSet.beforeFirst();
+
+            int i = 0;
+
+            while (resultSet.next()) {
+
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int cost = resultSet.getInt("cost");
+                int number = resultSet.getInt("number");
+                int categoryId = resultSet.getInt("category_id");
+                products[i] = new Product(id, name, cost, number, categoryId);
+                System.out.println(products[i]);
+                i++;
+            }
+
+            return products;
+        }
     }
 
 }
