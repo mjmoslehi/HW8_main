897dd68f8a6e5bd41e11b89f7a449161b44a95e9
mamad <you@example.com>
2024-02-09 08:33:08 +0330

    scanner to Menu class

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,9 +1,13 @@
 package menu;
 
+import java.util.Scanner;
+
 public class Menu {
 
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
     private static final String ANSI_GREEN = "\u001B[32m";
+
+    private final Scanner scanner = new Scanner(System.in);
 
 }
