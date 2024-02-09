d42d900ef8f8ce8b97ba6adab18a9afbe1f74b3c
mamad <you@example.com>
2024-02-09 08:25:27 +0330

    add singIn method to UserServiceImpel class

--- src/main/java/service/user/UserServiceImpel.java
+++ src/main/java/service/user/UserServiceImpel.java
@@ -4,10 +4,17 @@ import base.service.BaseServiceImpel;
 import model.User;
 import repository.user.UserRepository;
 
+import java.sql.SQLException;
+
 public class UserServiceImpel extends BaseServiceImpel<Integer , User, UserRepository> implements UserService {
 
     public UserServiceImpel(UserRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public Boolean singIn(String username, String password) throws SQLException {
+        return repository.singIn(username,password);
     }
 
 }
