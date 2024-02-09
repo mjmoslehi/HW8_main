4f0c7e1648f64edfe1dee547b5e36269076931c7
mamad <you@example.com>
2024-02-09 08:17:50 +0330

    create CategoryServiceImpel class

---
+++ src/main/java/service/category/CategoryServiceImpel.java
@@ -0,0 +1,8 @@
+package service.category;
+
+import base.service.BaseServiceImpel;
+import model.Category;
+import repository.category.CategoryRepository;
+
+public class CategoryServiceImpel extends BaseServiceImpel<Integer, Category, CategoryRepository> implements CategoryService {
+}
