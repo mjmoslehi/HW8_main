6a283ad4f3d246d787d3d1150df7a0e0b34e6254
mamad <you@example.com>
2024-02-09 08:20:06 +0330

    add checkCount method to ProductService interface

--- src/main/java/service/product/ProductService.java
+++ src/main/java/service/product/ProductService.java
@@ -8,4 +8,6 @@ import java.sql.SQLException;
 public interface ProductService extends BaseService<Integer, Product> {
 
     Product[] loadAll() throws SQLException;
+
+    int checkCount(int id)throws SQLException;
 }
