9e154d3da242027a973e3ed289a832c9bcc55320
mamad <you@example.com>
2024-02-09 08:13:32 +0330

    add costOfCart method to CartService interface

--- src/main/java/service/cart/CartService.java
+++ src/main/java/service/cart/CartService.java
@@ -3,5 +3,9 @@ package service.cart;
 import base.service.BaseService;
 import model.Cart;
 
+import java.sql.SQLException;
+
 public interface CartService extends BaseService<Integer , Cart> {
+
+    int costOfCart(int userId) throws SQLException;
 }
