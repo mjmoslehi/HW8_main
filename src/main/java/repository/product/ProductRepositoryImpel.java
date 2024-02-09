bb86d329feb9a918dbee0f406b95be02b95f43fb
mamad <you@example.com>
2024-02-09 07:59:48 +0330

    add constructor ProductRepositoryImpel class

--- src/main/java/repository/product/ProductRepositoryImpel.java
+++ src/main/java/repository/product/ProductRepositoryImpel.java
@@ -3,5 +3,12 @@ package repository.product;
 import base.repository.BaseRepositoryImpel;
 import model.Product;
 
+import java.sql.Connection;
+
 public class ProductRepositoryImpel  extends BaseRepositoryImpel<Integer , Product> implements ProductRepository {
+
+    public ProductRepositoryImpel(Connection connection) {
+        super(connection);
+    }
+
 }
