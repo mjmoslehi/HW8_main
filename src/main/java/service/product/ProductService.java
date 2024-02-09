9efc55c9fa7b7538295fb05b963d6cd8f21e2f0c
mamad <you@example.com>
2024-02-09 08:19:55 +0330

    add loadAll method to ProductService interface

--- src/main/java/service/product/ProductService.java
+++ src/main/java/service/product/ProductService.java
@@ -3,5 +3,9 @@ package service.product;
 import base.service.BaseService;
 import model.Product;
 
+import java.sql.SQLException;
+
 public interface ProductService extends BaseService<Integer, Product> {
+
+    Product[] loadAll() throws SQLException;
 }
