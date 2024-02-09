b1220f27c15324145645e6d941a69b28445539ce
mamad <you@example.com>
2024-02-09 08:15:24 +0330

    add costOfCart method to CartServiceImpel class

--- src/main/java/service/cart/CartServiceImpel.java
+++ src/main/java/service/cart/CartServiceImpel.java
@@ -4,10 +4,16 @@ import base.service.BaseServiceImpel;
 import model.Cart;
 import repository.cart.CartRepository;
 
+import java.sql.SQLException;
+
 public class CartServiceImpel extends BaseServiceImpel<Integer , Cart, CartRepository> implements CartService {
 
     public CartServiceImpel(CartRepository repository) {
         super(repository);
     }
 
+    @Override
+    public int costOfCart(int userId) throws SQLException {
+        return repository.costOfCart(userId);
+    }
 }
