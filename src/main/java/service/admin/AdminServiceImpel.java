7f88870581241b3779babcf108ff0b604a92d4d6
mamad <you@example.com>
2024-02-09 08:11:45 +0330

    create AdminServiceImpel class

---
+++ src/main/java/service/admin/AdminServiceImpel.java
@@ -0,0 +1,8 @@
+package service.admin;
+
+import base.service.BaseServiceImpel;
+import model.Admin;
+import repository.admin.AdminRepository;
+
+public class AdminServiceImpel extends BaseServiceImpel<Integer, Admin, AdminRepository> implements AdminService{
+}
