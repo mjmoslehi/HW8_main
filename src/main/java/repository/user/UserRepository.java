a300f030e11dfb45464115550d98355a2f0504c0
mamad <you@example.com>
2024-02-09 08:03:15 +0330

    add singIn method UserRepository interface

--- src/main/java/repository/user/UserRepository.java
+++ src/main/java/repository/user/UserRepository.java
@@ -3,5 +3,9 @@ package repository.user;
 import base.repository.BaseRepository;
 import model.User;
 
+import java.sql.SQLException;
+
 public interface UserRepository extends BaseRepository<Integer, User> {
+
+    Boolean singIn(String username, String password) throws SQLException;
 }
