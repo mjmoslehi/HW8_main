d9d021e61292bf76d91c291b0405b48363e52619
mamad <you@example.com>
2024-02-09 07:38:41 +0330

    add singIn method to AdminRepository interface

--- src/main/java/repository/admin/AdminRepository.java
+++ src/main/java/repository/admin/AdminRepository.java
@@ -3,5 +3,9 @@ package repository.admin;
 import base.repository.BaseRepository;
 import model.Admin;
 
+import java.sql.SQLException;
+
 public interface AdminRepository  extends BaseRepository<Integer , Admin> {
+
+    boolean singIn (String username , String password) throws SQLException;
 }
