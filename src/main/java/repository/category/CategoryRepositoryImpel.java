9638e6e9a90df40cccdc0e0b79cf6cc7a441f6be
mamad <you@example.com>
2024-02-09 07:56:02 +0330

    add constructor to CategoryRepositoryImpel class

--- src/main/java/repository/category/CategoryRepositoryImpel.java
+++ src/main/java/repository/category/CategoryRepositoryImpel.java
@@ -3,5 +3,11 @@ package repository.category;
 import base.repository.BaseRepositoryImpel;
 import model.Category;
 
+import java.sql.Connection;
+
 public class CategoryRepositoryImpel  extends BaseRepositoryImpel<Integer , Category> implements CategoryRepository{
+
+    public CategoryRepositoryImpel(Connection connection) {
+        super(connection);
+    }
 }
