c21fc490a60733c566a83c4bce69d3d1c5829aa6
mamad <you@example.com>
2024-02-09 08:13:50 +0330

    add loadAllUserChoices method to CartService interface

--- src/main/java/service/cart/CartService.java
+++ src/main/java/service/cart/CartService.java
@@ -8,4 +8,6 @@ import java.sql.SQLException;
 public interface CartService extends BaseService<Integer , Cart> {
 
     int costOfCart(int userId) throws SQLException;
+
+    Cart [] loadAllUserChoices(int userId) throws SQLException;
 }
