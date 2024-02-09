1286020599c29cc60ebb813ff2fefe9a1225da8e
mamad <you@example.com>
2024-02-09 07:47:56 +0330

    add costOfCart method to CartRepository interface

--- src/main/java/repository/cart/CartRepository.java
+++ src/main/java/repository/cart/CartRepository.java
@@ -3,5 +3,9 @@ package repository.cart;
 import base.repository.BaseRepository;
 import model.Cart;
 
+import java.sql.SQLException;
+
 public interface CartRepository extends BaseRepository<Integer , Cart> {
+
+    int costOfCart(int userId) throws SQLException;
 }
