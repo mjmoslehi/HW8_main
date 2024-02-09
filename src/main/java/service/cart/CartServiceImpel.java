abe1404c04169e216cfd32e1f9874514b87e6a4b
mamad <you@example.com>
2024-02-09 08:14:52 +0330

    create CartServiceImpel class

---
+++ src/main/java/service/cart/CartServiceImpel.java
@@ -0,0 +1,11 @@
+package service.cart;
+
+import base.service.BaseServiceImpel;
+import model.Cart;
+import repository.cart.CartRepository;
+
+public class CartServiceImpel extends BaseServiceImpel<Integer , Cart, CartRepository> implements CartService {
+
+
+
+}
