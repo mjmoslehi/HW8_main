256b8f47f197b3767d26502a76fc29d894c3bead
mamad <you@example.com>
2024-02-09 08:11:09 +0330

    add singIn method to AdminService interface

--- src/main/java/service/admin/AdminService.java
+++ src/main/java/service/admin/AdminService.java
@@ -3,5 +3,8 @@ package service.admin;
 import base.service.BaseService;
 import model.Admin;
 
+import java.sql.SQLException;
+
 public interface AdminService extends BaseService<Integer , Admin> {
+    boolean singIn (String username , String password) throws SQLException;
 }
