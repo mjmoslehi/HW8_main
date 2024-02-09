4f869534be3a8e75a3fd8c37dd526c8e64beaab3
mamad <you@example.com>
2024-02-09 08:00:56 +0330

    add mapResultSetToEntity method ProductRepositoryImpel class

--- src/main/java/repository/product/ProductRepositoryImpel.java
+++ src/main/java/repository/product/ProductRepositoryImpel.java
@@ -5,6 +5,7 @@ import model.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ProductRepositoryImpel  extends BaseRepositoryImpel<Integer , Product> implements ProductRepository {
@@ -35,5 +36,18 @@ public class ProductRepositoryImpel  extends BaseRepositoryImpel<Integer , Produ
         preparedStatement.setInt(3,entity.getNumber());
         preparedStatement.setInt(4,entity.getCategoryId());
     }
+
+    @Override
+    public Product mapResultSetToEntity(ResultSet resultSet) throws SQLException {
+
+        Product product = new Product();
+        product.setId(resultSet.getInt("id"));
+        product.setName(resultSet.getString("name"));
+        product.setCost(resultSet.getInt("cost"));
+        product.setNumber(resultSet.getInt("number"));
+        product.setCategoryId(resultSet.getInt("category_id"));
+        return product;
+    }
+
 
 }
