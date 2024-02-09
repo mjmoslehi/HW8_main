9972aa19df5d2a2d8bd1791e79e07fbfe1c9fdf2
mamad <you@example.com>
2024-02-09 08:22:59 +0330

    add singIn method to UserService interface

--- src/main/java/service/user/UserService.java
+++ src/main/java/service/user/UserService.java
@@ -3,5 +3,9 @@ package service.user;
 import base.service.BaseService;
 import model.User;
 
+import java.sql.SQLException;
+
 public interface UserService extends BaseService<Integer , User> {
+
+    Boolean singIn(String username, String password) throws SQLException;
 }
