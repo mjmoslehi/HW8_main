d8227c478ab8a5146627d85ddaf6fa6aff4b1010
mamad <you@example.com>
2024-02-09 07:58:10 +0330

    add ProductRepository interface

---
+++ src/main/java/repository/product/ProductRepository.java
@@ -0,0 +1,10 @@
+package repository.product;
+
+import base.repository.BaseRepository;
+import model.Product;
+
+public interface ProductRepository extends BaseRepository<Integer, Product> {
+
+
+
+}
