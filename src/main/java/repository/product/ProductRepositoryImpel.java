9914aaef20c762d7e37a13b469621cbeca356ff9
mamad <you@example.com>
2024-02-09 07:59:27 +0330

    create ProductRepositoryImpel class

---
+++ src/main/java/repository/product/ProductRepositoryImpel.java
@@ -0,0 +1,7 @@
+package repository.product;
+
+import base.repository.BaseRepositoryImpel;
+import model.Product;
+
+public class ProductRepositoryImpel  extends BaseRepositoryImpel<Integer , Product> implements ProductRepository {
+}
