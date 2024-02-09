ffd7dc1381d3cd862b6fc3361a15ae6ce3f2b5ef
mamad <you@example.com>
2024-02-09 08:12:22 +0330

    add singIn method to AdminServiceImpel class

--- src/main/java/service/admin/AdminServiceImpel.java
+++ src/main/java/service/admin/AdminServiceImpel.java
@@ -4,9 +4,16 @@ import base.service.BaseServiceImpel;
 import model.Admin;
 import repository.admin.AdminRepository;
 
+import java.sql.SQLException;
+
 public class AdminServiceImpel extends BaseServiceImpel<Integer, Admin, AdminRepository> implements AdminService{
 
     public AdminServiceImpel(AdminRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public boolean singIn(String username, String password) throws SQLException {
+        return repository.singIn(username,password);
     }
 }
