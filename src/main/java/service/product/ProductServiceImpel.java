cdd3fee174a6894b6329a6fb6a534a388e6e24e6
mamad <you@example.com>
2024-02-09 08:21:40 +0330

    add loadAll method to ProductServiceImpel class

--- src/main/java/service/product/ProductServiceImpel.java
+++ src/main/java/service/product/ProductServiceImpel.java
@@ -4,10 +4,17 @@ import base.service.BaseServiceImpel;
 import model.Product;
 import repository.product.ProductRepository;
 
+import java.sql.SQLException;
+
 public class ProductServiceImpel extends BaseServiceImpel<Integer, Product, ProductRepository> implements ProductService {
 
     public ProductServiceImpel(ProductRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public Product[] loadAll() throws SQLException {
+        return repository.loadAll();
     }
 
 }
