8899d32fbdb7208594d34938ad3a35d9f10631a3
mamad <you@example.com>
2024-02-09 08:04:10 +0330

    create UserRepositoryImpel class

---
+++ src/main/java/repository/user/UserRepositoryImpel.java
@@ -0,0 +1,7 @@
+package repository.user;
+
+import base.repository.BaseRepositoryImpel;
+import model.User;
+
+public class UserRepositoryImpel  extends BaseRepositoryImpel<Integer, User> implements UserRepository{
+}
