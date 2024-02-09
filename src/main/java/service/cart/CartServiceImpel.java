e4716c5c0842920f9f20c3156640c700f2aa0685
mamad <you@example.com>
2024-02-09 08:15:07 +0330

    add constructor to CartServiceImpel class

--- src/main/java/service/cart/CartServiceImpel.java
+++ src/main/java/service/cart/CartServiceImpel.java
@@ -6,6 +6,8 @@ import repository.cart.CartRepository;
 
 public class CartServiceImpel extends BaseServiceImpel<Integer , Cart, CartRepository> implements CartService {
 
-
+    public CartServiceImpel(CartRepository repository) {
+        super(repository);
+    }
 
 }
