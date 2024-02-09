3a5bc5cfcaaa05e29b371ea3e281527d7d2e31eb
mamad <you@example.com>
2024-02-09 08:25:11 +0330

    add constructor to UserServiceImpel class

--- src/main/java/service/user/UserServiceImpel.java
+++ src/main/java/service/user/UserServiceImpel.java
@@ -5,4 +5,9 @@ import model.User;
 import repository.user.UserRepository;
 
 public class UserServiceImpel extends BaseServiceImpel<Integer , User, UserRepository> implements UserService {
+
+    public UserServiceImpel(UserRepository repository) {
+        super(repository);
+    }
+
 }
