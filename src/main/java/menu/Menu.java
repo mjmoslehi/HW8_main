82a7279f196fb63eedc843e431e2d522d037a31b
mamad <you@example.com>
2024-02-09 08:32:48 +0330

    add color fields to Menu class

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,4 +1,9 @@
 package menu;
 
 public class Menu {
+
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+
 }
