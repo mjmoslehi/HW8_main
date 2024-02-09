de4973c54373af8436b599e117f011ce9cfdfa62
mamad <you@example.com>
2024-02-09 08:28:38 +0330

    add cart table to tables.sql

--- src/main/java/sql/tables.sql
+++ src/main/java/sql/tables.sql
@@ -30,3 +30,13 @@ create table if not exists product
     category_id integer,
     foreign key (category_id) references category (id)
 );
+
+create table if not exists cart
+(
+    id            serial primary key,
+    user_id       integer,
+    product_id    integer,
+    count_product integer,
+    foreign key (user_id) references users (id),
+    foreign key (product_id) references product (id)
+);
