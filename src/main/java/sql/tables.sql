be7ecf6bef7fd151715a02b2a6abb4eb7f5d449f
mamad <you@example.com>
2024-02-09 08:28:26 +0330

    add product table to tables.sql

--- src/main/java/sql/tables.sql
+++ src/main/java/sql/tables.sql
@@ -20,3 +20,13 @@ create table if not exists category
     id   serial primary key,
     name varchar
 );
+
+create table if not exists product
+(
+    id          serial primary key,
+    name        varchar,
+    cost        integer,
+    number      integer,
+    category_id integer,
+    foreign key (category_id) references category (id)
+);
