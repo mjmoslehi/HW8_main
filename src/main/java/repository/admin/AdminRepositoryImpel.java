34dcbea79a86b79ded364cdb5759a06b8643c1e3
mamad <you@example.com>
2024-02-09 07:40:45 +0330

    add constructor AdminRepositoryImpel class

--- src/main/java/repository/admin/AdminRepositoryImpel.java
+++ src/main/java/repository/admin/AdminRepositoryImpel.java
@@ -3,5 +3,11 @@ package repository.admin;
 import base.repository.BaseRepositoryImpel;
 import model.Admin;
 
+import java.sql.Connection;
+
 public class AdminRepositoryImpel extends BaseRepositoryImpel<Integer, Admin> implements AdminRepository{
+
+    public AdminRepositoryImpel(Connection connection) {
+        super(connection);
+    }
 }
