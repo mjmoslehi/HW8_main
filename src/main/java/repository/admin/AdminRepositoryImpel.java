a4c61e64a44278bc733323a09de3bcd5a83c0817
mamad <you@example.com>
2024-02-09 07:40:02 +0330

    create AdminRepositoryImpel class

---
+++ src/main/java/repository/admin/AdminRepositoryImpel.java
@@ -0,0 +1,7 @@
+package repository.admin;
+
+import base.repository.BaseRepositoryImpel;
+import model.Admin;
+
+public class AdminRepositoryImpel extends BaseRepositoryImpel<Integer, Admin> implements AdminRepository{
+}
