3aba919e526d68c060c8588f014b56199680a95c
mamad <you@example.com>
2024-02-09 08:20:53 +0330

    create ProductServiceImpel class

---
+++ src/main/java/service/product/ProductServiceImpel.java
@@ -0,0 +1,8 @@
+package service.product;
+
+import base.service.BaseServiceImpel;
+import model.Product;
+import repository.product.ProductRepository;
+
+public class ProductServiceImpel extends BaseServiceImpel<Integer, Product, ProductRepository> implements ProductService {
+}
